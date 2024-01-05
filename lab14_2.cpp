b7368b97c47ec9aa83dc647e5added9e71f44522
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2024-01-05 15:49:43 +0700

    lab14_2.cpp completed

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -19,3 +19,34 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double mat[][N]) {
+  for (int i = 0; i < N; i++) {
+    cout << "Row " << i+1 << ": ";
+    for (int j = 0; j < N; j++) {
+      cin >> mat[i][j];
+    }
+  }
+}
+
+void findLocalMax(const double in[][N], bool out[][N]) {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      if (i == 0 || i == N-1 || j == 0 || j == N-1) {
+        out[i][j] = 0;
+        continue;
+      }
+       double curr = in[i][j];
+       out[i][j] = (curr >= in[i-1][j]) && (curr >= in[i+1][j]) && (curr >= in[i][j-1]) && (curr >= in[i][j+1]);
+    }
+  }
+}
+
+void showMatrix(const bool mat[][N]) {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      cout << mat[i][j] << ' ';
+    }
+    cout << '\n';
+  }
+}
